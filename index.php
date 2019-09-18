c17f66e6f0589ae841a68a337b3503f9ea0c6dfb
Luis Tapia <36052812+14030598@users.noreply.github.com>
2019-09-18 00:39:40 -0500

    iniciando

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+    //include 'componentes/header.php';
+ ?>
+
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>TREZE</title>
+    <link rel="stylesheet" href="css/login.css">
+    <link rel='stylesheet' href='http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>
+  </head>
+  <body>
+    <div class="caja">
+
+    	<div class="signup">
+    		<h2 class="form-title" id="signup"><span>or</span>Registrarse</h2>
+    		<div class="form-holder">
+    			<input type="text" class="input" placeholder="Nombre" />
+    			<input type="email" class="input" placeholder="Email" />
+    			<input type="password" class="input" placeholder="Password" />
+    		</div>
+    		<button class="submit-btn">Sign up</button>
+    	</div>
+    	<div class="login slide-up">
+    		<div class="center">
+    			<h2 class="form-title" id="login"><span>or</span>Iniciar sesion</h2>
+    			<div class="form-holder">
+    				<input type="email" class="input" placeholder="Email" />
+    				<input type="password" class="input" placeholder="Password" />
+    			</div>
+    			<button class="submit-btn">Log in</button>
+    		</div>
+    	</div>
+    </div>
+    <script  src="jss/script_form.js"></script>
+  </body>
+</html>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en" >
+
+<body>
+<!-- partial:index.partial.html -->
+
+<!-- partial -->
+
+
+</body>
+</html>
---
+++ inicio.php
@@ -0,0 +1,50 @@
+<?php
+    //include 'componentes/header.php';
+ ?>
+
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>TREZE</title>
+    <link rel="stylesheet" href="css/plantilla.css">
+    <link rel="stylesheet" href="boton_animado.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel='stylesheet' href='http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>
+  </head>
+  <body>
+    <header>
+
+    </header>
+    <p>hola</p>
+    
+      <button class="bubbly-button">Click me!</button>
+      <script  src="./script.js"></script>
+  </body>
+  <footer>
+    <div class="social-btns">
+        <a class="btn facebook" href="#">
+            <i class="fa fa-facebook"></i>
+        </a>
+        <a class="btn twitter" href="#">
+            <i class="fa fa-twitter"></i>
+        </a>
+        <a class="btn google" href="#">
+            <i class="fa fa-google"></i>
+        </a>
+        <a class="btn dribbble" href="#">
+            <i class="fa fa-dribbble"></i>
+        </a>
+        <a class="btn skype" href="#">
+            <i class="fa fa-skype"></i>
+        </a>
+    </div>
+
+  </footer>
+</html>
+
+
+
+<?php
+    //include('layouts/footer.php');
+?>
